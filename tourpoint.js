f519775069fd8c491914ddd7fcb0186da5f7393f
getta16 <158020318+getta16@users.noreply.github.com>
2024-05-16 01:15:43 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+var tourpoint =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao yai national park",
+          "type": "natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            101.37213519339588,
+            14.438588013184557
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "grand palace",
+          "type": "palace",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.49258031925422,
+            13.751621563714593
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "iconsiam",
+          "type": "department store",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.5104364999981,
+            13.72671775350615
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "wat phrathat doi suthep",
+          "type": "temple",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.92138150000062,
+            18.804977593748433
+          ],
+          "type": "Point"
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "phra pathom chedi",
+          "type": "temple",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.06000170127129,
+            13.819533015406236
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "phi phi don island",
+          "type": "natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.77321089603242,
+            7.7399053467214145
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "phromthep cape",
+          "type": "natural",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.30311492589726,
+            7.758666422106984
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Khitchakut National Park",
+          "type": "natural",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            102.16209443038343,
+            12.84501886902379
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pattaya beach",
+          "type": "natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.88038000000074,
+            12.933197904073182
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "River Kwai Bridge",
+          "type": "structure",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            99.5036362785496,
+            14.040708345118816
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
